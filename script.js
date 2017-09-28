6431694091bc335ec2f835728df5f9d18a19e41b
Olivier Knell <olivier.knell@makina-corpus.com>
2017-09-28 10:57:55 +0200

    update table

--- script.js
+++ script.js
@@ -11,51 +11,51 @@ function loadJSON(callback, fileName) {
     xobj.send(null);  
  }
 
-
  function init(fileName) {
     loadJSON(function(response) {
-         var actual_JSON = {"not":"defined"}
+        var actual_JSON = {"not":"defined"};
         actual_JSON = JSON.parse(response);
-        showData(actual_JSON)
+        showData(actual_JSON);
         },
         fileName);
 }
 
 function showData(actual_JSON){
     console.log(actual_JSON);
+    console.log("coucou");
     var finalResult = "<table>";
     
     // creating the table header
     finalResult += "<tr>";
-    my_obj = actual_JSON[0];
-    for (var key in my_obj){
-        finalResult += "<th>" + key + "</th>"; 
-    }
+    
+    finalResult += "<th>Image</th>";
+    finalResult += "<th>Title</th>";
+    finalResult += "<th>Abstract</th>";
+    finalResult += "<th>Link</th>"; 
+    
     finalResult += "</tr>";
 
     // feeling the table with data
     for (var i in actual_JSON){
-        if(i>=100){break;}
-        obj = actual_JSON[i]
-        var myResult = "";
-        myResult += "<tr>";
+        if(i>=10){break;}
+        obj = actual_JSON[i];
+
+        var img = obj.img;
+        var title = obj.title;
+        var link = obj.link;
+        var abstract = obj.abstract;
+        var date = obj.date;
         
-        for (var key in obj){
-            var data = obj[key];
-            if (key == "img"){
-                data = "<img src='" + data + "'></img>";
-            }
-            if (key == "title"){
-                data = "<h3>" + data + "</h3>";
-            }
-            if (key == "link"){
-                data = "<a href='" + data + "'>" + data + "</a>";
-            }
-            myResult += "<td>" + data + "</td>";
-        }
+        var myResult = "<tr>"; 
+
+        myResult += "<td><img src='" + img + "'></img></td>";
+        myResult += "<td><h3>" + title + "</h3><p>" + date + "</p></td>";
+        myResult += "<td>" + abstract + "</td>";
+        myResult += "<td><a href='" + link + "'>" + link + "</a></td>";  
+
         myResult += "</tr>";
-        finalResult += myResult;
         
+        finalResult += myResult;
     }
     finalResult += "</table>";
     document.getElementById("data").innerHTML = finalResult;
