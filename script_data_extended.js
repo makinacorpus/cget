b908acbe34102bac952ecc22769eff88c6d5374b
Olivier Knell <olivier.knell@makina-corpus.com>
2017-10-13 14:24:06 +0200

    adapted js

--- script_data_extended.js
+++ script_data_extended.js
@@ -65,14 +65,14 @@ function showData(actual_JSON){
         myResult += "</td>";
         myResult += "<td><a href='" + obj.link + "' target='_blank'><img src='link.png'</img</a></td>";  
 
-        myResult += obj.area;
-        myResult += obj.key_words;
-        myResult += obj.contact;
-        myResult += obj.state;
-        myResult += obj.project_holder;
-        myResult += obj.partner;
-        myResult += obj.economic;
-        myResult += obj.video;
+        myResult += addDataIfExists(obj.area);
+        myResult += addDataIfExists(obj.key_words);
+        myResult += addDataIfExists(obj.contact);
+        myResult += addDataIfExists(obj.state);
+        myResult += addDataIfExists(obj.project_holder);
+        myResult += addDataIfExists(obj.partner);
+        myResult += addDataIfExists(obj.economic);
+        myResult += addDataIfExists(obj.video);
 
         myResult += "</tr>";
         
@@ -84,7 +84,7 @@ function showData(actual_JSON){
 
 function addDataIfExists(data){
     var result;
-    result += "<td>";
+    result = "<td>";
         if(data != null){
             result += data;
         } 
