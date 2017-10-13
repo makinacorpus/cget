58c4b45cec07650f66b2c24104c223f5b98116ce
Olivier Knell <olivier.knell@makina-corpus.com>
2017-10-13 10:49:16 +0200

    addapted js and html to the extended data files

--- index.md
+++ index.md
@@ -1,7 +1,7 @@
 <html>
 <head>
 <meta charset="utf-8"/>
-<script src="script.js"></script>
+<script src="script_data_extended.js"></script>
 <link rel="stylesheet" href="style.css" />
 	<title>CGET data page</title>
 </head>
@@ -10,14 +10,15 @@
 	<h3>Choose an option to see some data</h3>
 	<select onchange="init(this.options[this.selectedIndex].value);" 
 			onload="init(this.options[0].value);">
-		<option value="veolia">Veolia</option>
+		<!--<option value="veolia">Veolia</option>-->
 		<option value="solidarum">Solidarum</option>
-		<option value="vinci">Vinci</option>
-		<option value="carasso">Carasso</option>
-		<option value="apriles">Apriles</option>
-		<option value="bretagne_solidaire">Bretagne Creative</option>
-		<option value="unccas">Unccas</option>
+		<!--<option value="vinci">Vinci</option>-->
+		<!--<option value="carasso">Carasso</option>-->
+		<!--<option value="apriles">Apriles</option>-->
+		<option value="bretagne">Bretagne Creative</option>
+		<!--<option value="unccas">Unccas</option>-->
 		<option value="reseaurural">Reseau rural</option>
+		<option value="avise">Avise</option>
 	</select>
 	<div id="data"></div>
 </body>
---
+++ script_data_extended.js
@@ -0,0 +1,97 @@
+//Loading the JSON file
+function loadJSON(callback, fileName) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'data_extended/' + fileName.concat('.json'), true); 
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
+ function init(fileName) {
+    loadJSON(function(response) {
+        var actual_JSON = {"not":"defined"};
+        actual_JSON = JSON.parse(response);
+        showData(actual_JSON);
+        },
+        fileName);
+}
+
+//Creating the table and filling it with the given json file
+function showData(actual_JSON){
+    //console.log(actual_JSON);
+    var finalResult = "<table>";
+    
+    // creating the table header
+    finalResult += "<tr>";
+    
+    finalResult += "<th>Image</th>";
+    finalResult += "<th>Title</th>";
+    finalResult += "<th>Abstract</th>";
+    finalResult += "<th>Link</th>"; 
+    finalResult += "<th>Area</th>"; 
+    finalResult += "<th>Key words</th>"; 
+    finalResult += "<th>Contact</th>"; 
+    finalResult += "<th>State (finnished)</th>"; 
+    finalResult += "<th>Project holder</th>"; 
+    finalResult += "<th>Partner</th>"; 
+    finalResult += "<th>Economic model</th>"; 
+    finalResult += "<th>Video</th>"; 
+    
+    finalResult += "</tr>";
+
+    // filling the table with data
+    for (var i in actual_JSON){
+        if(i>=100){break;} //Limit for tests
+        obj = actual_JSON[i];
+
+        var img = obj.img;
+        var title = obj.title;
+        var link = obj.link;
+        var abstract = obj.abstract;
+        var date = obj.date;
+        
+        var myResult = "<tr>"; 
+
+        myResult += "<td><img src='" + img + "'></img></td>";
+        myResult += "<td><h3>" + title + "</h3><p>";
+        if(date != null){
+            myResult += date;
+        }  
+        myResult += "</p></td>";
+        myResult += "<td>";
+        if(abstract != null){
+            myResult += abstract;
+        } 
+        myResult += "</td>";
+        myResult += "<td><a href='" + link + "' target='_blank'><img src='link.png'</img</a></td>";  
+
+        myResult += addDataIfExists(obj.area);
+        myResult += addDataIfExists(obj.key_words);
+        myResult += addDataIfExists(obj.contact);
+        myResult += addDataIfExists(obj.state);
+        myResult += addDataIfExists(obj.project_holder);
+        myResult += addDataIfExists(obj.partner);
+        myResult += addDataIfExists(obj.economic);
+        myResult += addDataIfExists(obj.video);
+
+        myResult += "</tr>";
+        
+        finalResult += myResult;
+    }
+    finalResult += "</table>";
+    document.getElementById("data").innerHTML = finalResult;
+}
+
+function addDataIfExists(data){
+    result += "<td>";
+        if(data != null){
+            result += data;
+        } 
+    result += "</td>";
+    return result
+}
